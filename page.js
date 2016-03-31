98098ab51e4437f437ce48fa67f5332169f6e0d0
gumybear <bsdusa10@BSD-10.local>
2016-03-31 14:12:22 -0400

    added js

---
+++ page.js
@@ -0,0 +1,14 @@
+ var x = 6;
+
+ function football() {
+   x = x + 1;
+
+  alert (x);
+ }
+
+ function troll(){
+   
+   var butt = document.getElementById("trollbutt");
+    butt.classList.toggle("move")
+      
+      }
